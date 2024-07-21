9e4aecde92a5100e98e67150f84f550cb8dc9166
Avashneupane9857 <juisch20220011252@gmail.com>
2024-07-22 03:37:07 +0530

    :bugs: Bugs fixed for docker-compose file

--- Docker-CH/identidock/app/identidock.py
+++ Docker-CH/identidock/app/identidock.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-	return 'Hello World!\n'
+default_name = 'Joe Bloggs'
+
+@app.route('/', methods=['GET', 'POST'])
+def get_identicon():
+    name = default_name
+    if request.method == 'POST':
+        name = request.form.get('name', default_name)
+
+    header = '<html><head><title>Identidock</title></head><body>'
+    body = '''<form method="POST">
+              Hello <input type="text" name="name" value="{}">
+              <input type="submit" value="Submit">
+              </form>
+              <p>You look like a:
+              <img src="/monster/monster.png"/>
+              '''.format(name)
+    footer = '</body></html>'
+
+    return header + body + footer
+
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=5003)
+    app.run(debug=True, host='0.0.0.0', port=5002)
 
