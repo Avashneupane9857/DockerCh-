493dd71cae878199c21a97716b7af490474b48a3
Avashneupane9857 <juisch20220011252@gmail.com>
2024-07-23 04:07:27 +0530

    :wrench: commandFileSetUp

--- Docker-CH/identidock/cmd.sh
+++ Docker-CH/identidock/cmd.sh
@@ -3,6 +3,9 @@ set -e
 if [ "$ENV" = "DEV" ]; then
     echo "this is development server"
     exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running Unit Tests"
+    exec python "tests.py"
 else
     echo "this is the production server"
     exec uwsgi --http 0.0.0.0:5002 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
