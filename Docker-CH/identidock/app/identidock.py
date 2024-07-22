af8df358c34038d2d326ce8132837d8373ba965f
Avashneupane9857 <juisch20220011252@gmail.com>
2024-07-23 03:25:10 +0530

    :whale: CH-7 ends

--- Docker-CH/identidock/app/identidock.py
+++ Docker-CH/identidock/app/identidock.py
@@ -1,8 +1,9 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
-
+import redis
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
@@ -29,9 +30,15 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
+
+ image = cache.get(name)
+ if image is None:
+    print ("Cache miss", flush=True)
     r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
     image = r.content
-    return Response(image, mimetype='image/png')
+    cache.set(name, image)
+
+ return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5002)
